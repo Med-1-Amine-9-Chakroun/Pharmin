2adae7f5cce7c95e4ba27c349d64d58edac9aa8e
Med-1-Amine-9-Chakroun <chakrounmedamine19@gmail.com>
2023-04-25 01:15:43 +0100

    mooved work to htdocs php

---
+++ PHP/DbConnecion.php
@@ -0,0 +1,16 @@
+<?php
+//creer une instance du classe pdo et connecter au serveur
+//$bdd identifiant de la connection
+function maConnexion(){
+try
+{
+$bdd = new PDO('mysql:host=localhost;dbname=ateliersdeformation','root','');
+$bdd->query("SET NAMES 'utf8'");
+return $bdd;
+}
+catch (PDOException $e)
+{
+die('Erreur:'.$e->getMessage());
+}
+}
+?>
